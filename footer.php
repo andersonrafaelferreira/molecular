ffd0cd5d5aeb2c655a3ace53fdcaac93ff5e0ff8
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2020-05-14 01:23:14 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main div element.
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+?>
+
+		 </div> <!-- .row -->
+	</div><!-- #wrapper -->
+
+<!-- banner -->
+<section id="banner">
+	<div class="container">
+		<div class="col-md-6">
+			<h1>Agende  seu <strong>Teste RT-PCR,</strong> Padrão Ouro para o
+combate ao Covid – 19.</h1>
+			<p class="title">O único teste que detecta o vírus desde o <strong>primeiro dia</strong> de contágio.
+</p>
+<?php echo do_shortcode('[contact-form-7 id="187" title="molecular"]')?>
+		</div>
+		<div class="col-md-6">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/doutores.png" alt="">
+			<div class="clear"></div>
+			<div class="col-md-4">
+<img src="<?php echo get_template_directory_uri(); ?>/images/Phone.png" alt="">
+<h5>Agende pelo app</h5>
+<img src="<?php echo get_template_directory_uri(); ?>/images/appstore.png" alt="">
+<br>
+
+<br>
+<img src="<?php echo get_template_directory_uri(); ?>/images/playstore.png" alt="">
+			</div>
+			<div class="col-md-4">
+<img src="<?php echo get_template_directory_uri(); ?>/images/Whatsapp.png" alt="">
+<h5>Nos envie um WhatsApp</h5>
+<a href="">
+	<button>ENVIAR MENSAGEM</button>
+</a>
+			</div>
+			<div class="col-md-4">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/Phone2.png" alt="">
+				<h5>Ligue para nosso call-center</h5>
+				<a href="tel:0800 899 8650">0800 899 8650</a>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- banner END -->
+
+<!-- corona -->
+<section id="corona"></section>
+<!-- corona END -->
+
+<!-- passos -->
+<section id="passos">
+	<div class="container">
+	<div class="col-md-12">
+		<h1>Passo a passo para realização do teste</h1>
+		<p>O RT-PCR é feito por um sistema de Drive-Thru,
+		método mais seguro para evitar a contaminação durante a
+		realização do exame, pois garante que não haja contato
+		físico entre os pacientes. Durante todo o processo de
+		coleta da amostra, com duração média de 2 minutos,
+		o paciente permanece no interior do seu veículo.
+
+ </p>
+	</div>
+	<div class="clear"></div>
+		<div class="col-md-12" id="line">
+			<div class="twenty">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/01.png" alt="">
+				<h3>1º passo</h3>
+				<p>
+				Agende seu teste através do app,
+				site, WhatsApp
+				ou telefone.
+				</p>
+			</div>
+			<div class="twenty">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/02.png" alt="">
+				<h3>2º passo</h3>
+				<p>
+Você receberá
+a confirmação da data, horário e
+local do exame.
+				</p>
+			</div>
+			<div class="twenty">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/03.png" alt="">
+				<h3>3º passo</h3>
+				<p>
+Se dirija ao local indicado respeitando as informações recebidas.
+
+<span>OBS: Não é necessária nenhuma preparação
+prévia ao exame.</span>
+
+				</p>
+			</div>
+			<div class="twenty">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/04.png" alt="">
+				<h3>4º passo</h3>
+				<p>
+Os pacientes
+deverão permanecer em seus veículos durante toda a coleta,
+e a nossa equipe de laboratório realizará o procedimento
+seguindo as normas de saúde.
+				</p>
+			</div>
+			<div class="twenty">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/05.png" alt="">
+				<h3>5º passo</h3>
+				<p>
+Seu resultado
+será enviado em até
+4 horas após a realização do exame, na ferramenta utilizada para o agendamento.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- passos END -->
+
+<!-- dna -->
+<div id="dna"></div>
+<!-- teste -->
+<div class="col-md-12" id="preteste">
+	<h1>O Teste RT-PCR Padrão Ouro</h1>
+	<p>Tabela comparativa entre os 3 testes disponíveis no Brasil</p>
+</div>
+<section id="teste">
+	<div class="container">
+
+		<div class="col-md-3">
+			<div class="item">
+				<h3>Teste de Coronavírus</h3>
+			</div>
+			<div class="item">
+				<h3>O que é?</h3>
+			</div>
+			<div class="item">
+				<h3>Precisão e
+				quando fazer</h3>
+			</div>
+			<div class="item">
+				<h3>Coleta</h3>
+			</div>
+			<div class="item">
+				<h3>Reultado</h3>
+			</div>
+			<div class="item">
+				<h3>Amostras por dia</h3>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="item">
+				<h3 class="big-text">RT-PCR</h3>
+			</div>
+			<div class="item">
+				<p>Exame genético considerado o
+padrão-ouro no diagnóstico da COVID-19,
+por meio de técnicas de biologia molecular.
+
+</p>
+			</div>
+			<div class="item">
+				<p>Altíssima precisão
+			<strong>desde os primeiros dias de contágio</strong>.
+</p>
+			</div>
+			<div class="item">
+				<p>Secreções do fundo
+do nariz e da garganta.
+</p>
+			</div>
+			<div class="item">
+				<p>Até 4 horas.</p>
+			</div>
+			<div class="item">
+				<p>100-1000</p>
+			</div>
+			<div class="item">
+			<p>O <strong>RT-PCR</strong> é o <strong>TESTE OFICIAL</strong> recomendado pela
+<strong>OMS</strong> (Organização Mundial da Saúde) e pelo CDC
+(Centro de Prevenção e Controle de Doenças),
+para <strong>contenção do avanço da doença</strong>.
+</p>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="item">
+				<h3>Sorológico</h3>
+			</div>
+			<div class="item">
+				<p>Teste que verifica a resposta imunológica do organismo
+ao vírus, detectando a presença
+dos anticorpos IgM e IgG.
+</p>
+			</div>
+			<div class="item">
+				<p>Alta precisão
+					<strong>após o 10° da apresentação
+					dos primeiros sintomas</strong>.
+				</p>
+				<p class="red">
+					Chance de falsos negativos
+					quando feito antes disso devido
+					a ausência de anticorpos.
+				</p>
+			</div>
+			<div class="item">
+				<p>Sangue.</p>
+			</div>
+			<div class="item">
+				<p>Em até 2 horas.</p>
+			</div>
+			<div class="item">
+				<p>10-100</p>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="item">
+				<h3>Antígeno</h3>
+			</div>
+			<div class="item">
+				<p>Teste rápido que demonstra
+a presença de proteínas
+próprias do vírus na amostra.
+
+</p>
+			</div>
+			<div class="item">
+				<p>Alta precisão
+<strong>após o 5º de apresentação
+dos primeiros sintomas,
+na fase aguda da doença</strong>.
+
+</p>
+ <p class="red">Chance de falsos negativos
+quando feito antes disso devido
+a baixa sensibilidade.</p>
+
+			</div>
+			<div class="item">
+				<p>Secreções do fundo
+do nariz e da garganta.
+</p>
+			</div>
+			<div class="item">
+				<p>Até 30 minutos.</p>
+			</div>
+			<div class="item">
+				<p><20</p>
+			</div>
+		</div>
+		<!-- <div class="col-md-3"></div>
+		<div class="col-md-3"></div> -->
+	</div>
+</section>
+<!-- teste END -->
+
+<!-- quem -->
+<section id="quem">
+	<div class="container"><div class="col-md-12">
+		<h1>
+		Uma empresa do grupo ETG criada <br>
+exclusivamente para o combate ao Covid-19.
+
+		</h1>
+	</div>
+		<div class="col-md-6">
+			<h2>Quem Somos</h2>
+			<p>
+
+A ETG Company, é uma empresa de tecnologia e soluções globais para indústrias de montagens temporárias.
+</p><p>
+Com mais de 15 anos de experiência no mercado nacional e internacional de prestação de serviço, e seguindo nossa missão de proporcionar experiencias genuínas por meio de métodos inovadores, criamos a Teste Molecular, marca do grupo focada no controle epidemiológico via sistema drive thru para o combate ao COVID-19.
+</p><p>
+Nossa proposta é implementar um projeto modular e itinerante, para iniciar imediatamente a realização de exames em massa em áreas críticas e focos estratégicos, e atender assim a alta demanda de testes para detecção do SARS-COV-2 para diminuir o impacto econômico e social no BB.</p>
+		</div>
+		<div class="col-md-6">
+			<h2>Segurança no que fazemos</h2>
+			<p>
+
+Para garantir a segurança e eficácia do nosso método de coleta e testagem, formamos alianças internacionais com instituições pioneira na realização
+de testes PCR para COVID-19 no mundo e
+</p><p>
+Os testes são feitos em tempo real com alta sensibilidade e acurácia
+mínima de 99,6%.
+</p>
+	<div class="col-md-3">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/bullets.png" alt="">
+	</div>
+	<div class="col-md-9">
+	<ul id="lista">
+		<li>Detecção e identificação de genes alvos (RdRP, gene N)
+	específicos para COVID-19 e Sabercovírus (gene F) em um
+	única amostra.</li>
+		<li>Utilização do sistema UDG para previnir contaminação, ou seja, garantir que não haja resultado "falso positivo".</li>
+		<li>Interpretação automitzada de dados.</li>
+	</ul>
+	</div>
+		</div>
+	</div>
+</section>
+<!-- quem END -->
+
+<div class="clear" style="background: #f6f6f6; height: 60px">
+
+</div>
+
+	<footer>
+
+	<div class="container">
+		<div class="col-md-4 border-right">
+			<h2>Fale conosco</h2>
+			<p>Entre em contato conosco através do e-mail
+			<a href="mailto:contato@etg.company">contato@etg.company</a> ou pelo telefone <a href="tel:(11) 3000.3000">(11) 3000.3000</a></p>
+		</div>
+		<div class="col-md-8">
+			<div class="col-md-4">
+				<h2>Redes sociais</h2>
+				<nav>
+					<a href=""><i class="fab fa-facebook-f"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+					<a href=""><i class="fab fa-linkedin"></i></a>
+				</nav>
+			</div>
+			<div class="col-md-8">
+				<div class="col-md-7">
+					<p class="address">ADR BIOLOGIA MOLECULAR - TESTES MOLECULARES  <br>
+SERVIÇOS LABORATORIAIS LTDA | CNPJ: 12.498.977/0001-60 <br>
+Av. Jamaris, 100 - Conj. 510, Moema <br>
+São Paulo - SP</p>
+				</div>
+				<div class="col-md-5">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/etg_bottom.png" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	</footer>
+
+
+	<?php wp_footer(); ?>
+</body>
+</html>
